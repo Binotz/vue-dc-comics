7a1c9c621e6f9afec5b19f7924adfe87ba55a18d
Binotz <binotto.riccardo@gmail.com>
2022-07-06 16:04:41 +0200

    Added icons section

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
     <HeaderContent />
+    <MainComponent />
+    <CTAComponent />
   </div>
 </template>
 
 <script>
 import HeaderContent from './components/HeaderContent.vue'
+import MainComponent from './components/MainComponent.vue'
+import CTAComponent from './components/CTAComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderContent
+    HeaderContent,
+    MainComponent,
+    CTAComponent
   }
 }
 </script>
 
 <style lang="scss">
-@import './style/commons.scss'
+@import './style/commons.scss';
 </style>
